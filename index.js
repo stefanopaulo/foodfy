dac71e40f1a4d5f5eec6ac731f9719658833e6a4
stefanopaulo <stefanopaulo47@gmail.com>
2020-10-23 10:14:43 -0300

    caminho absoluto das views

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'njk');
 
-nunjucks.configure('views', {
+nunjucks.configure(path.join(__dirname, 'views'), {
     express: app,
     autoescape: false,
     noCache: true
