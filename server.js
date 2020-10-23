284402ceac1ddf5d7acbae8324cbf6227ed27b90
stefanopaulo <stefanopaulo47@gmail.com>
2020-10-23 10:02:01 -0300

    deploy

--- server.js
+++ server.js
@@ -6,9 +6,10 @@ const path = require('path');
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'njk');
 
-nunjucks.configure('views', path.join(__dirname, 'views'), {
+nunjucks.configure('views', {
     express: app,
     autoescape: false,
     noCache: true
